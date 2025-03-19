a8d9bc01bb9c14c156a21236b7186d370c3fd051
ivannissimrch <ivannissimrch@gmail.com>
2025-03-19 18:27:38 -0400

    Add basic structure to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
+    <section className=" h-full text-black flex flex-col items-center overflow-y-auto">
+      <h1 className="text-[#000] text-[64px] not-italic font-extrabold leading-[normal] mt-[100px]">
+        EMBER AI
+      </h1>
+      <p className="mt-[70px] text-[36px] ">Short description</p>
+      <button className="mt-[31px] w-[245px] h-[60px] bg-gray-300 rounded-4xl">
+        Start
+      </button>
+    </section>
   );
 }
