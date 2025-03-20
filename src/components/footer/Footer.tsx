18319cfea7629a6fba9ffca22e2cc9e17dd06948
NatnaelSisay <natyjava8@gmail.com>
2025-03-20 13:07:32 -0400

    Fix layout structure

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,23 +2,23 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Footer() {
-  return (
-    <footer className="bg-[hsla(212,_50%,_87%,_1)] text-black [grid-area:12_/_1_/_13_/_13] flex items-end pl-[31px] justify-evenly pb-[7px]">
-      <Link
-        className=" w-[10%]"
-        href="https://github.com/chingu-voyages/V54-tier2-team-23"
-      >
-        <Image
-          src="images/githubIcon.svg"
-          alt="location icon"
-          width={45}
-          height={45}
-        />
-      </Link>
-      <p className=" font-[Bitter] text-[26px] not-italic font-normal leading-[normal] w-[80%] flex justify-center">
-        ©2025 Ember AI. All Rights Reserved
-      </p>
-      <div className=" w-[10%]"></div>
-    </footer>
-  );
+	return (
+		<footer className="bg-[hsla(212,_50%,_87%,_1)] text-black [grid-area:12_/_1_/_13_/_13] flex items-center pl-[31px] justify-center pb-[7px] gap-2">
+			<Link
+				className=""
+				href="https://github.com/chingu-voyages/V54-tier2-team-23"
+			>
+				<Image
+					src="images/githubIcon.svg"
+					alt="location icon"
+					width={25}
+					height={25}
+				/>
+			</Link>
+
+			<p className=" font-[Bitter] not-italic font-normal leading-[normal] flex justify-center">
+				©2025 Ember AI. All Rights Reserved
+			</p>
+		</footer>
+	);
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,15 @@
 import Link from "next/link";
 
 export default function Header() {
-  return (
-    <header className="bg-white text-black [grid-area:1_/_1_/_2_/_13] flex items-center ">
-      <Link href={"/"}>
-        <h1 className="text-[#E26310] font-[Poppins] text-[22px] not-italic font-semibold leading-[normal]  pl-[31px]">
-          Logo
-        </h1>
-      </Link>
-      <Link href={"AboutUs"}>
-        <h2 className="text-[#61529D] font-[Helvetica_Neue] text-[20px] not-italic font-medium leading-[24px] ml-[89PX]">
-          ABOUT US
-        </h2>
-      </Link>
-    </header>
-  );
+	return (
+		<header className="bg-white text-black [grid-area:1_/_1_/_2_/_13] flex items-center px-6 py-3 justify-between">
+			<div className="logo">
+				<Link href={"/"}>
+					<h1 className="font-[Poppins] text-[22px] not-italic font-semibold leading-[normal] ">
+						Logo
+					</h1>
+				</Link>
+			</div>
+		</header>
+	);
 }
