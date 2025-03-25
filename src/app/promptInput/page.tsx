a85c89111f9d409c1b689e589d78cd1191461afd
ivannissimrch <ivannissimrch@gmail.com>
2025-03-25 19:58:40 -0400

    Hide scroll bar on prompt input page

--- src/app/promptInput/page.tsx
+++ src/app/promptInput/page.tsx
@@ -38,7 +38,7 @@ export default function PromptInput() {
   }
 
   return (
-    <div className="h-full border-white overflow-y-scroll">
+    <div className="h-full border-white overflow-y-hidden">
       <header className="bg-white text-black  mx-auto w-full px-4 py-6 sm:px-6 lg:px-8 absolute flex justify-center ">
         O-O-O-O-O
       </header>
