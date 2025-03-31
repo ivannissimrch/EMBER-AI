17a23a2434495a90155fffcb13eee5239d0174d8
NatnaelSisay <natyjava8@gmail.com>
2025-03-31 19:18:35 -0400

    send prompt and recive response from Gemini

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -2,12 +2,16 @@
 import { useEffect, useState } from "react";
 import { useStoreContext } from "../helpers/StoreContext";
 import Link from "next/link";
+import { generateResponse } from "@/util/api";
+import { tree } from "next/dist/build/templates/app-page";
 
 export default function Review() {
 	const { storeValue } = useStoreContext();
 
 	const [combineInputs, setCombileInputs] = useState<string>("");
 	const [disabled, setDisabled] = useState<boolean>(true);
+	const [geminiResonse, setGeminiResponse] = useState<string>("");
+	const [loading, setLoading] = useState<boolean>(false);
 
 	useEffect(() => {
 		const result = storeValue.inputValues
@@ -15,6 +19,13 @@ export default function Review() {
 			.join("\n");
 		setCombileInputs(result);
 	}, []);
+
+	const handleSubmit = async () => {
+		setLoading(true);
+		const response = await generateResponse(combineInputs);
+		setLoading(false);
+		setGeminiResponse(response as string);
+	};
 
 	return (
 		<section className="text-black flex flex-col items-center h-full overflow-y-auto">
@@ -41,9 +52,24 @@ export default function Review() {
 					</button>
 				</div>
 			</section>
-			<button className="mt-[80px] w-[245px] h-[60px] bg-gray-300 ">
+
+			<button
+				className="mt-[80px] w-[245px] h-[60px] bg-gray-300 cursor-pointer"
+				onClick={handleSubmit}
+			>
 				Generate Prompt
 			</button>
+
+			<div className="flex flex-col my-[62px] ">
+				<h3 className="mb-4">Ember Response</h3>
+				<div className=" w-[1080px] p-4 bg-gray-300  rounded-4xl">
+					{geminiResonse ? (
+						<p>{geminiResonse}</p>
+					) : (
+						<p>{loading ? "Loading.." : "Ask your question in the prompt"}</p>
+					)}
+				</div>
+			</div>
 		</section>
 	);
 }
