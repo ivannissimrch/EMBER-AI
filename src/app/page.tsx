4284ded3e2393d78047dbb869ffbb02be16630c2
ivannissimrch <ivannissimrch@gmail.com>
2025-04-23 19:09:42 -0400

    Update spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,18 @@ export default function Home() {
                         src="../../images/logo.svg"
                         alt="Ember AI logo"
                         width={468}
-                        height={150}
+                        height={78}
                         sizes="(max-width: 639px) 297px, (min-width: 640px) 468px"
-                        className="pb-16"
+                        className="mb-16 sm:mb-[86px]"
                     />
-                    <p className=" mb-2 w-[260px] text-center font-[Open_Sans] text-[20px] not-italic font-semibold leading-[24px] sm:text-[35px] sm:w-[600px] h-auto md:w-[995px] sm:leading-[38px] sm:mb-12">
+                    <p className=" mb-4 w-[260px] text-center font-[Open_Sans] text-[20px] not-italic font-semibold leading-[24px] sm:text-[35px] sm:w-[600px] h-auto md:w-[995px] sm:leading-[38px] sm:mb-12">
                         Meet your new prompt creation partner!
                     </p>
-                    <div className='text-center font-["Open_Sans"] text-[18px]  not-italic font-normal leading-[24px] sm:text-[33px] sm:leading-9 sm:w-[600px] md:max-4xl lg:w-auto lg:mb-12'>
+                    <div className='mb-16 text-center font-["Open_Sans"] text-[18px]  not-italic font-normal leading-[24px]  sm:text-[33px] sm:leading-9 sm:w-[600px]  md:max-4xl lg:w-auto lg:mb-24'>
                         See how the Pentagram Framework can take your prompt from Ember to flame.
                     </div>
                     <Link href="/promptInput">
-                        <button className="cursor-pointer mt-[31px] w-[174px] h-[38px] bg-[#61529D] rounded-4xl text-[white] text-[16px] font-[Helvetica_Neue] sm:w-[287px] sm:h-[64px]">
+                        <button className="mb-4 cursor-pointer  w-[174px] h-[38px] bg-[#61529D] rounded-4xl text-[white] text-[16px] font-[Helvetica_Neue] sm:w-[287px] sm:h-[64px] sm:mb-9">
                             Try It Out
                         </button>
                     </Link>
@@ -29,7 +29,7 @@ export default function Home() {
             </div>
             <Link
                 href="https://medium.com/knowledge-keeper/the-pentagram-framework-for-prompt-engineering-9c6c891e800b"
-                className='text-[#61529D] text-center font-["Helvetica_Neue"] text-[16px] not-italic font-normal leading-[normal] mt-9  sm:text-[28px]'
+                className='text-[#61529D] text-center font-["Helvetica_Neue"] text-[16px] not-italic font-normal leading-[normal]   sm:text-[28px]'
             >
                 Learn More about the Pentagram Framework
             </Link>
