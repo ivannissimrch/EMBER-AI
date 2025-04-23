2a296796186867d48c7cb60b5afb14604ec1b3ad
ivannissimrch <ivannissimrch@gmail.com>
2025-04-23 16:11:54 -0400

    Add responsiveness to logo at home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,34 +3,36 @@ import Link from 'next/link';
 
 export default function Home() {
     return (
-        <section className=" h-full text-black flex flex-col items-center justify-center overflow-y-auto">
+        <section className=" h-full text-black flex flex-col items-center justify-center overflow-y-auto flex-wrap">
             <div className="w-9/12 flex gap-2 items-center justify-center">
                 <div className="justify-items-center ">
                     <Image
                         src="../../images/logo.svg"
                         alt="Ember AI logo"
-                        width={297}
-                        height={77}
-                        // className="h-[150px] pb-[60px]"
-                        className="pb-[60px]"
+                        width={468}
+                        height={150}
+                        sizes="(max-width: 639px) 297px, (min-width: 640px) 468px"
+                        className="pb-16"
                     />
-                    <div className=" mb-2 w-[260px] text-center  font-[Open_Sans] text-[20px] not-italic font-semibold leading-[24px]">
+                    <p className=" mb-2 w-[260px] text-center font-[Open_Sans] text-[20px] not-italic font-semibold leading-[24px] sm:text-[35px] sm:w-[600px] h-auto md:w-[995px] sm:leading-[38px] sm:mb-12">
                         Meet your new prompt creation partner!
-                    </div>
-                    <div className='text-center font-["Open_Sans"] text-[18px] not-italic font-normal leading-[24px]'>
+                    </p>
+                    <div className='text-center font-["Open_Sans"] text-[18px]  not-italic font-normal leading-[24px] sm:text-[33px] sm:leading-9 sm:w-[600px] md:max-4xl lg:w-auto lg:mb-12'>
                         See how the Pentagram Framework can take your prompt from Ember to flame.
                     </div>
                     <Link href="/promptInput">
-                        <button className="cursor-pointer mt-[31px] w-[174px] h-[38px] bg-[#61529D] rounded-4xl text-[white] text-[16px] font-[Helvetica_Neue]">
+                        <button className="cursor-pointer mt-[31px] w-[174px] h-[38px] bg-[#61529D] rounded-4xl text-[white] text-[16px] font-[Helvetica_Neue] sm:w-[287px] sm:h-[64px]">
                             Try It Out
                         </button>
                     </Link>
                 </div>
-
-                {/* <div className="w-1/2 flex justify-center items-center">
-					<h2 className="font-[Poppins] text-4xl">logo</h2>å
-				</div> */}
             </div>
+            <Link
+                href="https://medium.com/knowledge-keeper/the-pentagram-framework-for-prompt-engineering-9c6c891e800b"
+                className='text-[#61529D] text-center font-["Helvetica_Neue"] text-[16px] not-italic font-normal leading-[normal] mt-9  sm:text-[28px]'
+            >
+                Learn More about the Pentagram Framework
+            </Link>
         </section>
     );
 }
