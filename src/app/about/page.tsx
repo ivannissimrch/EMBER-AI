762c6936f41c1faa8441f65b9fcc7cf11e15debd
Bryan Hoyem <bjhoyem@gmail.com>
2025-04-25 08:47:13 -0500

    Updated formatting for About Us page to match design for both desktop and mobile sizes.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,11 +8,11 @@ import React from 'react';
 export default function AboutPage() {
     return (
         <div className="text-black">
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center items-center mt-[58px]">
                 <h3 className="text-center font-bold text-2xl mb-[100px]">About Us</h3>
-                <section className="grid grid-cols-1 sm:grid-cols-2 max-w-[1293px] gap-x-[17px] items-center">
-                    <div className="h-auto m-[20px] rounded-[75px] text-[24px] bg-[#61529D] text-[white]">
-                        <p className="p-10">
+                <section className="grid grid-cols-1 sm:grid-cols-2 max-w-[1293px] gap-x-[58px] items-center mx-[43px]">
+                    <div className="max-w-[549px] h-auto rounded-[75px] text-[24px] bg-[#61529D] text-[white] mb-[44px]">
+                        <p className="p-10 text-[16px] md:text-[24px]">
                             Ember AI is an assistant app designed to help a user craft a well
                             written, complete prompt to use when utilizing an AI platform in seeking
                             information.
@@ -22,37 +22,37 @@ export default function AboutPage() {
                             which is composed of five parts.
                         </p>
                     </div>
-                    <div className="text-[1.5rem]">
-                        <div className="flex items-center">
-                            <img src="/images/group1.svg" className="h-[100%] pr-[35px]" />
+                    <div className="text-[16px] md:text-[24px]">
+                        <div className="flex items-center md:mb-[33px]">
+                            <img src="/images/group1.svg" className="h-[30px] md:h-[58px] pr-[17px]" />
                             <p>
                                 The Persona defines a role that the LLM, or AI, should assume to
                                 give a point of reference for context of it&apos;s response.
                             </p>
                         </div>
-                        <div className="flex  items-center">
-                            <img src="/images/group2.svg" className="h-[100%] pr-[35px]" />
+                        <div className="flex  items-center md:mb-[33px]">
+                            <img src="/images/group2.svg" className="h-[30px] md:h-[58px] pr-[17px]" />
                             <p>
                                 The Context gives background information for the LLM to use so that
                                 the desired task intent is understood.
                             </p>
                         </div>
-                        <div className="flex  items-center">
-                            <img src="/images/group3.svg" className="h-[100%] pr-[35px]" />
+                        <div className="flex  items-center md:mb-[33px]">
+                            <img src="/images/group3.svg" className="h-[30px] md:h-[58px]  pr-[17px]" />
                             <p>
                                 The Task should clearly define what the user is asking of the LLM
                                 and should include specific action verbs and instructions.
                             </p>
                         </div>
-                        <div className="flex  items-center">
-                            <img src="/images/group4.svg" className="h-[100%] pr-[35px]" />
+                        <div className="flex  items-center md:mb-[33px]">
+                            <img src="/images/group4.svg" className="h-[30px] md:h-[58px]  pr-[17px]" />
                             <p>
                                 The Output should describe the desired format of the response from
                                 the LLM.
                             </p>
                         </div>
-                        <div className="flex  items-center">
-                            <img src="/images/group5.svg" className="h-[100%] pr-[35px]" />
+                        <div className="flex  items-center md:mb-[33px]">
+                            <img src="/images/group5.svg" className="h-[30px] md:h-[58px]  pr-[17px]" />
                             <p>
                                 The Constraint should list any boundaries or restrictions for the
                                 LLM response or describe desired tone if applicable.
